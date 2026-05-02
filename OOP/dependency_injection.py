213b015547386d43533b923397aff448216d814f
AndrewMZ6 <Andreika3981@gmail.com>
2026-05-02 11:19:37 +0700

    refactor: add DIP from wikipedia and run ruff
    Add dependency inversion principle definition from wikipedia to the beginning of the file

--- OOP/dependency_injection.py
+++ OOP/dependency_injection.py
@@ -1,37 +1,47 @@
 from abc import ABC, abstractmethod
 
+"""
+From wikipedia:
+
+In object-oriented design, the dependency inversion principle is a specific methodology for loosely coupled software modules. When following this principle, 
+the conventional dependency relationships established from high-level, policy-setting modules to low-level, dependency modules are reversed, 
+thus rendering high-level modules independent of the low-level module implementation details. The principle states:
+
+1. High-level modules should not import anything from low-level modules. Both should depend on abstractions (e.g., interfaces).
+2. Abstractions should not depend on details. Details (concrete implementations) should depend on abstractions.
+By dictating that both high-level and low-level objects must depend on the same abstraction, this design principle inverts the way some people may think about
+ object-oriented programming.
+"""
+
 
 class Switchable(ABC):
-    
     @abstractmethod
     def turn_on(self):
-        '''Turns on the Switchable object'''
+        """Turns on the Switchable object"""
 
     @abstractmethod
     def turn_off(self):
-        '''Turns off the Switchable object'''
+        """Turns off the Switchable object"""
 
 
 class Lightbulb(Switchable):
-
-    def turn_on(self)  -> None:
+    def turn_on(self) -> None:
         print(f"the {self.__class__.__name__} is turned on")
 
-    def turn_off(self)  -> None:
+    def turn_off(self) -> None:
         print(f"the {self.__class__.__name__} is turned off")
 
 
 class FairyLights(Switchable):
-    def turn_on(self)  -> None:
+    def turn_on(self) -> None:
         print(f"the {self.__class__.__name__} is turned on")
 
-    def turn_off(self)  -> None:
+    def turn_off(self) -> None:
         print(f"the {self.__class__.__name__} is turned off")
 
 
 class PowerSwitcher:
-
-    def __init__(self, l:Switchable) -> None:
+    def __init__(self, l: Switchable) -> None:
         self.l = l
         self.turned_on = False
 
@@ -42,7 +52,6 @@ class PowerSwitcher:
         else:
             self.l.turn_on()
             self.turned_on = True
-
 
 
 f = FairyLights()
