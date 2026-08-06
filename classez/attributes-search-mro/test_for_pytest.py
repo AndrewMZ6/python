f88e80fd10bd5e22b709ac423a2155a5335cc6d3
AndrewMZ6 <Andreika3981@gmail.com>
2026-08-06 15:02:24 +0700

    add test for pytest

---
+++ classez/attributes-search-mro/test_for_pytest.py
@@ -0,0 +1,7 @@
+from test1 import A
+
+
+def test_proble():
+    a = A()
+    assert a.flare == 46
+    assert a.justice == 7778
--- classez/attributes-search-mro/test_test1.py
+++ classez/attributes-search-mro/test_test1.py
@@ -7,7 +7,7 @@ class MyTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.a = A()
-    
+
     def test_one(self):
         self.assertEqual(self.a.flare, 46)
 
@@ -16,7 +16,7 @@ class MyTest(unittest.TestCase):
 
     def test_three(self):
         with self.assertRaises(ZeroDivisionError):
-            1/0
+            1 / 0
 
 
 if __name__ == "__main__":
